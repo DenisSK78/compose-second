77e6ec706a428c789554911e686fef8f8d873361
Denis Kalinin <k-d-s@mail.ru>
2020-12-12 15:45:52 +0300

    refactoring

--- src/serv/Main.java
+++ src/serv/Main.java
@@ -37,9 +37,9 @@ public class Main {
         server.start();
     }
 
-    private static final String FIRST_LINK = "<p><a href='' onclick='goTo(1)'>Open first link 9001</a></p>";
-    private static final String SECOND_LINK = "<p><a href='' onclick='goTo(2)'>Open second link 9002</a></p>";
-    private static final String MAIN_LINK = "<p><a href='' onclick='goTo(0)'>Open main link 9000</a></p>";
+    private static final String FIRST_LINK = "<p><a onclick='goTo(1)'>Open first link 9001</a></p>";
+    private static final String SECOND_LINK = "<p><a onclick='goTo(2)'>Open second link 9002</a></p>";
+    private static final String MAIN_LINK = "<p><a onclick='goTo(0)'>Open main link 9000</a></p>";
 
     private static final String LINK_BODY = "<!DOCTYPE HTML >\n" +
             "<html lang='html'>\n" +
